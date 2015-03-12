77ffc98e4a718259a12997d9d48b3a52bc808fc0
Felipe Lopes Lage <lipelopeslage@hotmail.com>
2015-03-12 20:50:06 -0300

    Create clear_that_list.js
    Call the function treatColumns(), it will immediately search for <ul> or <ol> elements, list their <li> and find for bootstrap column classes and treat according to the screen's resolution.

---
+++ clear_that_list.js
@@ -0,0 +1,31 @@
+function treatColumns(){
+		var total = 12, i;
+		
+		function columns(num, col){
+			$("ul, ol").each(function(){
+				$(this).children("li.col-"+col+"-"+num).each(function(idx, itm){
+					var item = $(itm), x = 12/num;
+					if((idx) % x == 0 && idx > 0){
+						//console.log(idx+" >>",idx+1, x, item.index())
+						item.css("clear","both");
+					}else{
+						item.css("clear","none");
+					}
+				});
+			});				
+		}
+
+		if(winW < 768){
+			for(i=1; i <= total;i++)
+				columns(i, "xs");
+		}else if(winW >= 768 && winW < 992){
+			for(i=1; i <= total;i++)
+				columns(i, "sm");
+		}else if(winW >= 992 && winW < 1199){
+			for(i=1; i <= total;i++)
+				columns(i, "md");
+		}else if(winW >= 1200){
+			for(i=1; i <= total;i++)
+				columns(i, "lg");
+		}
+	}
